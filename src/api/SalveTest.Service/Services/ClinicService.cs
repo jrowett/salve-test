634d1063d6e7275674dddfba2fe51aa4c472b0a4
Jon Rowett <jon.rowett@alphamindsolutions.com>
2022-08-30 12:34:44 +0100

    check clinics data is present before executing query

--- src/api/SalveTest.Service/Services/ClinicService.cs
+++ src/api/SalveTest.Service/Services/ClinicService.cs
@@ -9,7 +9,14 @@ namespace SalveTest.Service.Services
     {
         public async Task<IEnumerable<Clinic>> GetClinicsAsync()
         {
-            using (var textReader = new StreamReader("SampleData\\clinics.csv"))
+            const string csv = "SampleData\\clinics.csv";
+
+            if (!File.Exists(csv))
+            {
+                throw new FileNotFoundException("Clinics data is missing. Please ensure the CSV file is present in the deployment directory");
+            }
+
+            using (var textReader = new StreamReader(csv))
             using(var csvReader = new CsvReader(textReader, CultureInfo.InvariantCulture))
             {
                 csvReader.Context.RegisterClassMap<ClinicMap>();
